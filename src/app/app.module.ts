129da5dac82ee7fe7a5102312c1dcceb2fb57954
JaRodriguez97 <cristi542012@gmail.com>
2022-11-10 10:37:07 -0500

    spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { AgmCoreModule } from '@agm/core';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FooterModule } from '@components/Footer/footer.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -9,6 +9,8 @@ import { NgxLocalstorageDirectiveModule } from 'ngx-localstorage';
 import { SwiperModule } from 'swiper/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,7 +26,10 @@ import { AppComponent } from './app.component';
       apiKey: 'AIzaSyANPMMk_LmctJkbG61fjRkLJ5HQHLqYEwc',
     }),
     NgxLocalstorageDirectiveModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' }),
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/Landing/landing.component.ts
+++ src/app/components/Landing/landing.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { userInterface } from '@models/users.interface';
 import { AppComponent } from '@app/app.component';
 import { LocalStorageService } from 'ngx-localstorage';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-landing',
@@ -13,10 +14,18 @@ export class LandingComponent implements OnInit {
 
   constructor(
     private appComponent: AppComponent,
-    private localStorageService: LocalStorageService
+    private localStorageService: LocalStorageService,
+    private spinner: NgxSpinnerService
   ) {}
 
   ngOnInit(): void {
-    this.appComponent.user = this.localStorageService.get('user', {})!;
+    this.spinner
+      .show()
+      .then(() => {
+        this.appComponent.user = this.localStorageService.get('user', {})!;
+      })
+      .then(() => {
+        this.spinner.hide();
+      });
   }
 }
